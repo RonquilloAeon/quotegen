abeb3ab6a51d2ef81ae03cc8812dbcf813aa989a
Martin R <23411718+RonquilloAeon@users.noreply.github.com>
2020-06-13 14:17:43 -0500

    Add postgres persistence

--- src/main/kotlin/org/ronquilloaeon/Hello.kt
+++ src/main/kotlin/org/ronquilloaeon/Hello.kt
@@ -1,10 +1,33 @@
 package org.ronquilloaeon
 
 import org.ronquilloaeon.domain.QuoteRepository
+import org.ronquilloaeon.domain.QuoteDBPersistence
 
 fun main(args: Array<String>) {
-    val quote = QuoteRepository().getRandom()
-    val author = if (quote.author.isBlank()) "unknown" else quote.author
+    // Set up db
+    val database = System.getenv("DB_NAME")
+    val username = System.getenv("DB_USER")
+    val password = System.getenv("DB_PASS")
 
-    println("Your random quote is by $author:\n${quote.text}")
+    val persistence = QuoteDBPersistence(database, username, password)
+    val quoteRepository = QuoteRepository(persistence)
+    val quotes = quoteRepository.list()
+
+    println("All quotes:")
+    quotes.forEach {
+        it.print()
+        println()
+    }
+
+    var doContinue = true
+
+    while (doContinue) {
+        print("Get a random quote? (y/n) ")
+        doContinue = readLine().toString() == "y"
+
+        quoteRepository.getRandom().print()
+        println()
+    }
+
+    persistence.close()
 }
